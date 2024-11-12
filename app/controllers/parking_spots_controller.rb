12854e19737215fb64462999c21377dd51340862
Michelle <pllmchllssn@gmail.com>
2024-11-12 11:27:50 +0800

    feat: leave method and optimizations

--- app/controllers/parking_spots_controller.rb
+++ app/controllers/parking_spots_controller.rb
@@ -22,21 +22,37 @@ class ParkingSpotsController < ApplicationController
   def edit
   end
 
-  def update
-  end
-
   def destroy
     parking_spot = ParkingSpot.find(params[:id])
-    parking_spot.destroy
-    redirect_to parking_spots_path, notice: "Parking spot removed."
+    if parking_spot.car_id != nil
+      car = Car.find(parking_spot.car_id)
+      parking_spot.destroy
+      car.destroy
+    end
+    redirect_to root_path
   end
 
   def park
     empty_spot = ParkingSpot.find_empty
-    car = Car.create(license: params[:license], color: params[:color])
-    if car.save
-      empty_spot.update(car: car)
-      redirect_to parking_spots_path
+    if !empty_spot.respond_to?("length")
+      car = Car.create(license: params[:license], color: params[:color])
+      if car.save
+        empty_spot.update(car: car)
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path, notice: "Parking lot full"
     end
+
+  end
+
+  def leave
+    parking_spot = ParkingSpot.find(params[:id])
+    if parking_spot.car_id != nil
+      car = Car.find(parking_spot.car_id)
+      parking_spot.update(car_id: nil)
+      car.destroy
+    end
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :parking_spots, only: [:index, :create, :destroy] do
-    post 'park', on: :collection
+    collection do
+      post 'park'
+    end
+    member do
+      delete 'leave'
+    end
   end
   root "parking_spots#index"
 end
